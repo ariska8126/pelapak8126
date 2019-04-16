35fb6bd6c6ff13adbd89cc5293601a4499c0be09
ariska8126 <ariska8126@students.amikom.ac.id>
2019-04-16 17:45:27 +0700

    register form done

---
+++ app/src/main/java/com/example/pelapak8126/Activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.pelapak8126.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pelapak8126.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
